72f11b35d9677d6b7b1055b376b4fad76269bb50
zeno <654452591@qq.com>
2021-05-20 18:34:06 +0800

    修改部分BUG

--- RandomPasswordGenerator/Form1.Designer.cs
+++ RandomPasswordGenerator/Form1.Designer.cs
@@ -29,6 +29,7 @@ namespace RandomPasswordGenerator
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.ckb_hasUpperCase = new System.Windows.Forms.CheckBox();
             this.ckb_hasLowerCase = new System.Windows.Forms.CheckBox();
             this.ckb_hasNumber = new System.Windows.Forms.CheckBox();
@@ -39,7 +40,7 @@ namespace RandomPasswordGenerator
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             this.tb_passwordLength = new System.Windows.Forms.TextBox();
-            this.tb_passowrdNumber = new System.Windows.Forms.TextBox();
+            this.tb_passowrdCount = new System.Windows.Forms.TextBox();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.ckb_hasSpecialCharacter = new System.Windows.Forms.CheckBox();
             this.btn_generate = new System.Windows.Forms.Button();
@@ -47,7 +48,7 @@ namespace RandomPasswordGenerator
             this.panel1 = new System.Windows.Forms.Panel();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.tb_passwordList = new System.Windows.Forms.RichTextBox();
+            this.lb_passwordList = new System.Windows.Forms.ListBox();
             this.tableLayoutPanel1.SuspendLayout();
             this.groupBox1.SuspendLayout();
             this.panel1.SuspendLayout();
@@ -61,10 +62,10 @@ namespace RandomPasswordGenerator
             this.ckb_hasUpperCase.Checked = true;
             this.ckb_hasUpperCase.CheckState = System.Windows.Forms.CheckState.Checked;
             this.ckb_hasUpperCase.Dock = System.Windows.Forms.DockStyle.Right;
-            this.ckb_hasUpperCase.Location = new System.Drawing.Point(222, 41);
+            this.ckb_hasUpperCase.Location = new System.Drawing.Point(222, 34);
             this.ckb_hasUpperCase.Name = "ckb_hasUpperCase";
             this.ckb_hasUpperCase.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.ckb_hasUpperCase.Size = new System.Drawing.Size(15, 32);
+            this.ckb_hasUpperCase.Size = new System.Drawing.Size(15, 25);
             this.ckb_hasUpperCase.TabIndex = 0;
             this.ckb_hasUpperCase.UseVisualStyleBackColor = true;
             // 
@@ -74,10 +75,10 @@ namespace RandomPasswordGenerator
             this.ckb_hasLowerCase.Checked = true;
             this.ckb_hasLowerCase.CheckState = System.Windows.Forms.CheckState.Checked;
             this.ckb_hasLowerCase.Dock = System.Windows.Forms.DockStyle.Right;
-            this.ckb_hasLowerCase.Location = new System.Drawing.Point(222, 79);
+            this.ckb_hasLowerCase.Location = new System.Drawing.Point(222, 65);
             this.ckb_hasLowerCase.Name = "ckb_hasLowerCase";
             this.ckb_hasLowerCase.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.ckb_hasLowerCase.Size = new System.Drawing.Size(15, 32);
+            this.ckb_hasLowerCase.Size = new System.Drawing.Size(15, 25);
             this.ckb_hasLowerCase.TabIndex = 1;
             this.ckb_hasLowerCase.UseVisualStyleBackColor = true;
             // 
@@ -90,7 +91,7 @@ namespace RandomPasswordGenerator
             this.ckb_hasNumber.Location = new System.Drawing.Point(222, 3);
             this.ckb_hasNumber.Name = "ckb_hasNumber";
             this.ckb_hasNumber.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.ckb_hasNumber.Size = new System.Drawing.Size(15, 32);
+            this.ckb_hasNumber.Size = new System.Drawing.Size(15, 25);
             this.ckb_hasNumber.TabIndex = 2;
             this.ckb_hasNumber.UseVisualStyleBackColor = true;
             // 
@@ -100,7 +101,7 @@ namespace RandomPasswordGenerator
             this.label1.Dock = System.Windows.Forms.DockStyle.Left;
             this.label1.Location = new System.Drawing.Point(3, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(37, 38);
+            this.label1.Size = new System.Drawing.Size(37, 31);
             this.label1.TabIndex = 5;
             this.label1.Text = "数字";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -109,9 +110,9 @@ namespace RandomPasswordGenerator
             // 
             this.label2.AutoSize = true;
             this.label2.Dock = System.Windows.Forms.DockStyle.Left;
-            this.label2.Location = new System.Drawing.Point(3, 38);
+            this.label2.Location = new System.Drawing.Point(3, 31);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(65, 38);
+            this.label2.Size = new System.Drawing.Size(65, 31);
             this.label2.TabIndex = 6;
             this.label2.Text = "大写字母";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -120,9 +121,9 @@ namespace RandomPasswordGenerator
             // 
             this.label3.AutoSize = true;
             this.label3.Dock = System.Windows.Forms.DockStyle.Left;
-            this.label3.Location = new System.Drawing.Point(3, 76);
+            this.label3.Location = new System.Drawing.Point(3, 62);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(65, 38);
+            this.label3.Size = new System.Drawing.Size(65, 31);
             this.label3.TabIndex = 7;
             this.label3.Text = "小写字母";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -131,9 +132,9 @@ namespace RandomPasswordGenerator
             // 
             this.label4.AutoSize = true;
             this.label4.Dock = System.Windows.Forms.DockStyle.Left;
-            this.label4.Location = new System.Drawing.Point(3, 114);
+            this.label4.Location = new System.Drawing.Point(3, 93);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(65, 38);
+            this.label4.Size = new System.Drawing.Size(65, 31);
             this.label4.TabIndex = 8;
             this.label4.Text = "特殊字符";
             this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -142,9 +143,9 @@ namespace RandomPasswordGenerator
             // 
             this.label7.AutoSize = true;
             this.label7.Dock = System.Windows.Forms.DockStyle.Left;
-            this.label7.Location = new System.Drawing.Point(3, 152);
+            this.label7.Location = new System.Drawing.Point(3, 124);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(65, 38);
+            this.label7.Size = new System.Drawing.Size(65, 31);
             this.label7.TabIndex = 11;
             this.label7.Text = "密码长度";
             this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -153,9 +154,9 @@ namespace RandomPasswordGenerator
             // 
             this.label8.AutoSize = true;
             this.label8.Dock = System.Windows.Forms.DockStyle.Left;
-            this.label8.Location = new System.Drawing.Point(3, 190);
+            this.label8.Location = new System.Drawing.Point(3, 155);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(65, 43);
+            this.label8.Size = new System.Drawing.Size(65, 34);
             this.label8.TabIndex = 12;
             this.label8.Text = "密码数量";
             this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -163,27 +164,27 @@ namespace RandomPasswordGenerator
             // tb_passwordLength
             // 
             this.tb_passwordLength.Dock = System.Windows.Forms.DockStyle.Right;
-            this.tb_passwordLength.Location = new System.Drawing.Point(99, 155);
+            this.tb_passwordLength.Location = new System.Drawing.Point(99, 127);
             this.tb_passwordLength.Name = "tb_passwordLength";
             this.tb_passwordLength.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.tb_passwordLength.Size = new System.Drawing.Size(138, 28);
             this.tb_passwordLength.TabIndex = 15;
             // 
-            // tb_passowrdNumber
+            // tb_passowrdCount
             // 
-            this.tb_passowrdNumber.Dock = System.Windows.Forms.DockStyle.Right;
-            this.tb_passowrdNumber.Location = new System.Drawing.Point(99, 193);
-            this.tb_passowrdNumber.Name = "tb_passowrdNumber";
-            this.tb_passowrdNumber.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.tb_passowrdNumber.Size = new System.Drawing.Size(138, 28);
-            this.tb_passowrdNumber.TabIndex = 16;
+            this.tb_passowrdCount.Dock = System.Windows.Forms.DockStyle.Right;
+            this.tb_passowrdCount.Location = new System.Drawing.Point(99, 158);
+            this.tb_passowrdCount.Name = "tb_passowrdCount";
+            this.tb_passowrdCount.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.tb_passowrdCount.Size = new System.Drawing.Size(138, 28);
+            this.tb_passowrdCount.TabIndex = 16;
             // 
             // tableLayoutPanel1
             // 
             this.tableLayoutPanel1.ColumnCount = 2;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 60F));
-            this.tableLayoutPanel1.Controls.Add(this.tb_passowrdNumber, 1, 5);
+            this.tableLayoutPanel1.Controls.Add(this.tb_passowrdCount, 1, 5);
             this.tableLayoutPanel1.Controls.Add(this.tb_passwordLength, 1, 4);
             this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.ckb_hasUpperCase, 1, 1);
@@ -207,7 +208,7 @@ namespace RandomPasswordGenerator
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(240, 233);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(240, 189);
             this.tableLayoutPanel1.TabIndex = 17;
             // 
             // ckb_hasSpecialCharacter
@@ -216,17 +217,18 @@ namespace RandomPasswordGenerator
             this.ckb_hasSpecialCharacter.Checked = true;
             this.ckb_hasSpecialCharacter.CheckState = System.Windows.Forms.CheckState.Checked;
             this.ckb_hasSpecialCharacter.Dock = System.Windows.Forms.DockStyle.Right;
-            this.ckb_hasSpecialCharacter.Location = new System.Drawing.Point(222, 117);
+            this.ckb_hasSpecialCharacter.Location = new System.Drawing.Point(222, 96);
             this.ckb_hasSpecialCharacter.Name = "ckb_hasSpecialCharacter";
-            this.ckb_hasSpecialCharacter.Size = new System.Drawing.Size(15, 32);
+            this.ckb_hasSpecialCharacter.Size = new System.Drawing.Size(15, 25);
             this.ckb_hasSpecialCharacter.TabIndex = 17;
             this.ckb_hasSpecialCharacter.UseVisualStyleBackColor = true;
             // 
             // btn_generate
             // 
-            this.btn_generate.Location = new System.Drawing.Point(-1, 244);
+            this.btn_generate.Font = new System.Drawing.Font("思源黑体 CN Regular", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btn_generate.Location = new System.Drawing.Point(0, 229);
             this.btn_generate.Name = "btn_generate";
-            this.btn_generate.Size = new System.Drawing.Size(240, 39);
+            this.btn_generate.Size = new System.Drawing.Size(239, 39);
             this.btn_generate.TabIndex = 19;
             this.btn_generate.Text = "生成密码";
             this.btn_generate.UseVisualStyleBackColor = true;
@@ -237,9 +239,9 @@ namespace RandomPasswordGenerator
             this.groupBox1.Controls.Add(this.panel1);
             this.groupBox1.Font = new System.Drawing.Font("思源黑体 CN Heavy", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.groupBox1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(251, 318);
+            this.groupBox1.Size = new System.Drawing.Size(251, 303);
             this.groupBox1.TabIndex = 21;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "密码选项";
@@ -250,44 +252,51 @@ namespace RandomPasswordGenerator
             this.panel1.Controls.Add(this.tableLayoutPanel1);
             this.panel1.Location = new System.Drawing.Point(6, 29);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(239, 283);
+            this.panel1.Size = new System.Drawing.Size(239, 268);
             this.panel1.TabIndex = 0;
             // 
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.panel2);
             this.groupBox2.Font = new System.Drawing.Font("思源黑体 CN Heavy", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.groupBox2.Location = new System.Drawing.Point(269, 12);
+            this.groupBox2.Location = new System.Drawing.Point(260, 3);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(480, 318);
+            this.groupBox2.Size = new System.Drawing.Size(480, 303);
             this.groupBox2.TabIndex = 22;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "密码列表";
             // 
             // panel2
             // 
-            this.panel2.Controls.Add(this.tb_passwordList);
+            this.panel2.Controls.Add(this.lb_passwordList);
             this.panel2.Location = new System.Drawing.Point(6, 29);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(468, 283);
+            this.panel2.Size = new System.Drawing.Size(468, 265);
             this.panel2.TabIndex = 0;
             // 
-            // tb_passwordList
+            // lb_passwordList
             // 
-            this.tb_passwordList.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.tb_passwordList.Location = new System.Drawing.Point(0, 0);
-            this.tb_passwordList.Name = "tb_passwordList";
-            this.tb_passwordList.Size = new System.Drawing.Size(468, 283);
-            this.tb_passwordList.TabIndex = 1;
-            this.tb_passwordList.Text = "";
+            this.lb_passwordList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lb_passwordList.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lb_passwordList.FormattingEnabled = true;
+            this.lb_passwordList.ItemHeight = 19;
+            this.lb_passwordList.Location = new System.Drawing.Point(0, 0);
+            this.lb_passwordList.Margin = new System.Windows.Forms.Padding(8);
+            this.lb_passwordList.Name = "lb_passwordList";
+            this.lb_passwordList.Size = new System.Drawing.Size(468, 265);
+            this.lb_passwordList.TabIndex = 2;
+            this.lb_passwordList.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.lb_passwordList_MouseDoubleClick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(758, 338);
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(745, 311);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "随机密码生成器";
             this.tableLayoutPanel1.ResumeLayout(false);
@@ -312,15 +321,15 @@ namespace RandomPasswordGenerator
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.TextBox tb_passwordLength;
-        private System.Windows.Forms.TextBox tb_passowrdNumber;
+        private System.Windows.Forms.TextBox tb_passowrdCount;
         private System.Windows.Forms.Button btn_generate;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.RichTextBox tb_passwordList;
         private System.Windows.Forms.CheckBox ckb_hasSpecialCharacter;
+        private System.Windows.Forms.ListBox lb_passwordList;
     }
 }
 
--- RandomPasswordGenerator/Form1.cs
+++ RandomPasswordGenerator/Form1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace RandomPasswordGenerator
@@ -8,27 +9,34 @@ namespace RandomPasswordGenerator
 
     public partial class Form1 : Form
     {
+        // 97-122
         private bool hasLowerCase;
+        // 65-90
         private bool hasUpperCase;
+        // 48-57
         private bool hasNumber;
+        // 33-47
+        // 58-64
+        // 91-96
         private bool hasSpecialCharacter;
 
         private int passwordLength;
-        private int passwordNum;
+        private int passwordCount;
 
+        // 小写字符
         List<char> LowerCases = new List<char> { 'a', 'b', 'c', 'd', 'e', 'f', 'g',
                                                 'h', 'i', 'j', 'k', 'l', 'm', 'n',
                                                 'o', 'p', 'q', 'r', 's', 't', 'u',
                                                 'v', 'w', 'x', 'y', 'z' };
-
+        // 大写字符
         List<char> UpperCases = new List<char> { 'A', 'B', 'C', 'D', 'E', 'F', 'G',
                                                  'H', 'I', 'J', 'K', 'L', 'M', 'N',
                                                  'O', 'P', 'Q', 'R', 'S', 'T', 'U',
                                                  'V', 'W', 'X', 'Y', 'Z' };
-
+        // 数字字符
         List<char> Numbers = new List<char> { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-
-        List<char> SpecialCharacters = new List<char> { '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '?', '/', '+', '-' };
+        // 特殊字符
+        List<char> SpecialCharacters = new List<char> { '!', '@', '#', '$', '%', '&', '*', '(', ')', '?', '+' };
         public Form1()
         {
             InitializeComponent();
@@ -37,68 +45,93 @@ namespace RandomPasswordGenerator
             ckb_hasSpecialCharacter.Checked = true;
             ckb_hasUpperCase.Checked = true;
             tb_passwordLength.Text = "16";
-            tb_passowrdNumber.Text = "5";
+            tb_passowrdCount.Text = "5";
 
         }
 
         private void btn_generate_Click(object sender, EventArgs e)
         {
-            hasNumber = this.ckb_hasNumber.Checked;
-            hasLowerCase = this.ckb_hasLowerCase.Checked;
-            hasUpperCase = this.ckb_hasUpperCase.Checked;
-            hasSpecialCharacter = this.ckb_hasSpecialCharacter.Checked;
+            this.lb_passwordList.Items.Clear();
 
+            // 是否包含数字字符
+            hasNumber = this.ckb_hasNumber.Checked;
+            // 是否包含小写字母字符
+            hasLowerCase = this.ckb_hasLowerCase.Checked;
+            // 是否包含大写字母字符
+            hasUpperCase = this.ckb_hasUpperCase.Checked;
+            // 是否包含特殊字符
+            hasSpecialCharacter = this.ckb_hasSpecialCharacter.Checked;
+            // 密码长度
             passwordLength = int.Parse(this.tb_passwordLength.Text);
-            passwordNum = int.Parse(this.tb_passowrdNumber.Text);
+            //密码数量
+            passwordCount = int.Parse(this.tb_passowrdCount.Text);
+
+            // 密码列表
+            List<string> newPasswordList = new List<string>();
+            
+            for (int count = 0; count < passwordCount; count++)
+            {
+                // 单个密码字符列表
+                List<string> SinglePasswordCharList = new List<string>();
+
+                Random rm = new Random(Guid.NewGuid().GetHashCode());
+
+                List<char> charList = preparCharList(hasLowerCase, hasUpperCase, hasNumber, hasSpecialCharacter);
+                int ccount = charList.Count;
+                for (int i = 0; i < passwordLength; i++)
+                {
+                    int index = rm.Next(ccount);
+                    SinglePasswordCharList.Add(charList[index].ToString());
+                    charList.RemoveAt(index);
+                    ccount -= 1;
+                }
+                // 单个密码
+                string newSinglePassword = string.Join("", SinglePasswordCharList.ToArray());
+                newPasswordList.Add(newSinglePassword);
+            }
+
+            foreach(string str in newPasswordList)
+            {
+                this.lb_passwordList.Items.Add(str);
+
+            }
+        }
+
+        private void lb_passwordList_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            int index = this.lb_passwordList.SelectedIndex;
+            String str = this.lb_passwordList.Items[index].ToString();
+            
+            Clipboard.SetDataObject(str);
+            MessageBox.Show("密码”" + str+ "”已复制");
+        }
+
+        /**
+         * 准备密码字符集合
+         **/
+        private List<char> preparCharList(bool hasL,bool hasU,bool hasN,bool hasS)
+        {
 
             List<char> charactersList = new List<char>();
 
-            if (hasNumber)
+            if (hasN)
             {
                 charactersList.InsertRange(charactersList.Count, Numbers);
             }
-            if (hasLowerCase)
+            if (hasL)
             {
                 charactersList.InsertRange(charactersList.Count, LowerCases);
             }
-            if (hasUpperCase)
+            if (hasU)
             {
                 charactersList.InsertRange(charactersList.Count, UpperCases);
             }
-            if (hasSpecialCharacter)
+            if (hasS)
             {
                 charactersList.InsertRange(charactersList.Count, SpecialCharacters);
             }
 
-            List<string> newPasswordList = new List<string>();
-            
-            for (int num = 0; num < passwordNum; num++)
-            {
-                
-                List<string> newPasswordChar = new List<string>();
-
-                Random rm = new Random(Guid.NewGuid().GetHashCode());
-
-                List<char> cs = new List<char>(charactersList);
-                int count = cs.Count;
-                for (int i = 0; i < passwordLength; i++)
-                {
-                    //rm = new Random(Guid.NewGuid().GetHashCode());
-                    int index = rm.Next(count);
-                    Console.WriteLine(cs[index]);
-                    newPasswordChar.Add(cs[index].ToString());
-                    cs.RemoveAt(index);
-                    count -= 1;
-                }
-
-                string newPassword = string.Join("", newPasswordChar.ToArray());
-                newPasswordList.Add(newPassword);
-            }
-
-            string newPL = string.Join("\n\n", newPasswordList.ToArray());
-
-            this.tb_passwordList.Text = newPL;
-
+            return charactersList;
         }
     }
 }
